f814aa2e821c7c864a638b746d71a5f8e1c69c1c
Rolf <rolf.v.d.forst@fau.de>
2023-12-05 11:22:48 +0100

    Update rrze-plugin-test.php

--- rrze-plugin-test.php
+++ rrze-plugin-test.php
@@ -3,7 +3,7 @@
 /*
   Plugin Name: RRZE Plugin Test
   Plugin URI: https://github.com/rvdforst/rrze-plugin-test
-  Version: 1.0.27
+  Version: 1.0.28
   Description: RRZE Plugin Test.
   Author: rvdforst
   Author URI: https://github.com/rvdforst
